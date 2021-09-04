44dc88898ff69665ea4d22565d63d1168fdc3d1d
Devanshu Ruhela <devanshuruhela2.0@gmail.com>
2021-09-04 23:43:50 +0530

    play-pause set

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
+const video = document.querySelector('video');
+const progressrange = document.getElementsByClassName('progress-range')
+const progressbar = document.getElementsByClassName("progress-bar");
+const playbtn = document.getElementById('play-btn');
+const volumeicon = document.getElementById('volume-icon');
+
+const volumerange = document.getElementsByClassName("volume-range");
+const volumebar = document.getElementsByClassName("volume-bar");
+
+const currenttime = document.getElementsByClassName('time-elapsed');
+const duration = document.getElementsByClassName('time-duration');
+
+const fullscreen = document.getElementsByClassName('fullscreen');
 
 
 // Play & Pause //
 
+function videoended()
+{
+      playbtn.classList.remove("fa-pause");
+      playbtn.classList.add("fa-play");
+      playbtn.setAttribute("title", "Play");
+}
+function playandpause()
+{
+  if(video.paused)
+  {
+    playbtn.classList.add('fa-pause');
+    playbtn.classList.remove('fa-play');
+    playbtn.setAttribute('title' , 'Pause')
+    video.play();
+  }
+  else{
+    video.pause();
+    videoended();
+  }
+}
 
 
 // Progress Bar //
@@ -19,3 +52,7 @@
 // Fullscree //
 
 
+//event listeners
+playbtn.addEventListener('click' , playandpause);
+video.addEventListener('click' , playandpause);
+video.addEventListener('ended' , videoended);
